efc1967e7a6317a00ee3e0ae56241b911202c0e6
Tinqiu <vincent.brunet.vb@gmail.com>
2021-03-17 22:38:11 -0400

    small fixes for vs-code

--- src/main/java/com/crusadehelper/repositories/CrusadeForceRepository.java
+++ src/main/java/com/crusadehelper/repositories/CrusadeForceRepository.java
@@ -2,6 +2,8 @@ package com.crusadehelper.repositories;
 
 import com.crusadehelper.entities.CrusadeForce;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CrusadeForceRepository extends JpaRepository<CrusadeForce, Integer> {
 }
