eefa6b6c1cf61729533ce45f87116964574ecae1
Tinqiu <vincent.brunet.vb@gmail.com>
2021-03-20 09:24:53 -0400

    renaming class to fit conventions

--- src/main/java/com/crusadehelper/configuration/SwaggerConfiguration.java
+++ src/main/java/com/crusadehelper/configuration/SwaggerConfiguration.java
@@ -2,20 +2,15 @@ package com.crusadehelper.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMethod;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.builders.ResponseMessageBuilder;
-import springfox.documentation.schema.ModelRef;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class swaggerConfiguration {
+public class SwaggerConfiguration {
     @Bean
     public Docket api() {
         //https://www.baeldung.com/swagger-2-documentation-for-spring-rest-api
