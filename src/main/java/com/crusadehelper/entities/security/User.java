61c7941ad80540ee0c532adb351fe453de693f09
Hugo Trudel <hugotrudel30@gmail.com>
2021-03-20 21:21:09 -0400

    Added Users, roles and privileges

---
+++ src/main/java/com/crusadehelper/entities/security/User.java
@@ -0,0 +1,34 @@
+package com.crusadehelper.entities.security;
+
+import java.util.Collection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.JoinColumn;
+
+@Entity
+public class User {
+ 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private boolean enabled;
+    private boolean tokenExpired;
+
+    @ManyToMany 
+    @JoinTable( 
+        name = "users_roles", 
+        joinColumns = @JoinColumn(
+          name = "user_id", referencedColumnName = "id"), 
+        inverseJoinColumns = @JoinColumn(
+          name = "role_id", referencedColumnName = "id")) 
+    private Collection<Role> roles;
+}
